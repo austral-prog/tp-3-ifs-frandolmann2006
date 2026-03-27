a8467ca4811cbb18fe15c4cc831c76e3e6429ad1
frandolmann2006 <frankdolmann@gmail.com>
2026-03-27 10:42:16 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -19,3 +19,12 @@ def grades():
         Insuficiente
     """
     pass
+    nota=int(input())
+    if nota>=9:
+        print("Excelente")
+    elif nota>=7:
+        print("Bueno")
+    elif nota>=5:
+        print("Regular")
+    elif nota>=0:
+        print("Insuficiente")
