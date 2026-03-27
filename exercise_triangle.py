94a6064edad0fff707028eb6d3741dff98fc709f
frandolmann2006 <frankdolmann@gmail.com>
2026-03-27 17:58:57 -0300

    Update exercise_triangle.py

--- exercise_triangle.py
+++ exercise_triangle.py
@@ -19,7 +19,7 @@ def triangle():
     lado1=float(input())
     lado2=float(input())
     lado3=float(input())
-    if (lado1 + lado2)>lado3:
+    if (lado1 + lado2 > lado3) and (lado2 + lado3 > lado1)  and (lado1+lado3 > lado2):
         print("Los lados forman un triangulo valido")
-    elif (lado1 + lado2)<lado3:
-        print("Los lados no forman un triangulo")
+    else:
+        print("Los lados no forman un triangulo valido")
